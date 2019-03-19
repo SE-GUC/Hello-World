f15727f578753781dbeb31000b5750ad662f40f8
TheKareemOne <thekareem13@gmail.com>
2019-03-19 19:06:22 +0200

    Created Task Model Using MongoDB

--- models/Task.js
+++ models/Task.js
@@ -1,15 +1,46 @@
-class Task {
-    constructor(levelOfCommitment, experienceLevel, setOfSkills, monetaryCompensation, id) {
-        this.levelOfCommitment = levelOfCommitment;
-        this.experienceLevel = experienceLevel;
-        this.setOfSkills = setOfSkills;
-        this.monetaryCompensation = monetaryCompensation;
-        this.id = id;
-        this.applicants = [];
-        this.acceptedApplicants = [];
-        this.reviewed = false;
-        this.extra = [];
-    };
-};
+mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const taskSchema = new Schema({
+   application:{
+       type: Object.Types.ObjectId,
+       ref: 'applications'
+   },
+    levelOfCommitment: {
+       type: Number,
+        min: 1,
+        max: 5
+    },
+    experienceLevel: {
+       type: Number,
+        min: 1,
+        max: 5
+    },
+    skills: {
+       type: [String],
+        required: true
+    },
+    monetaryCompensation: {
+       type: Number,
+    },
+    applicants: [
+        {
+            member: {
+                type: Object.Types.ObjectId,
+                ref: 'members'
+            },
+            status: {
+                type: String,
+                default: 'pending'
+            }
+        }
+    ],
+    reviewed:{
+       type: Boolean,
+        default: false
+    },
+    extra: {
+       type: [String]
+    }
+});
 
-module.exports = Task;
+module.exports = Task = mongoose.model('tasks',taskSchema);
