dd30876f00f26a92c86d200070eb1909cf1d520e
G3far97 <ahmed.fouadgaafar@student.guc.edu.eg>
2019-03-19 08:59:29 +0200

    Update models/Admin Using MongoDB

--- models/Admin.js
+++ models/Admin.js
@@ -1,7 +1,19 @@
-class Admin{
+/*class Admin{
     constructor(name,id){
         this.name = name;
         this.id = id;
     }
 }
-module.exports = Admin;
+module.exports = Admin;*/
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+const AdminSchema = new schema({
+    name:{
+        type:string,
+        required:true
+    },
+    User:{
+        type:Schema.Type.ObjectId,
+        ref:'users'
+    }
+})
