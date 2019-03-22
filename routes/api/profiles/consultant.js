cb27dc793ce8d60f3451b1c71c597473e2a96c71
mahmoudbasha3 <mahmoudbashabsbs@gmail.com>
2019-03-22 18:23:04 +0200

    route for consulted finished , yet to be tested

--- routes/api/profiles/consultant.js
+++ routes/api/profiles/consultant.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const uuid = require('uuid');
+const uuid = require('mongoose');
 
 //Load Consultant Model
-const User = require('../../../models/User');
-const Organization = require('../../../models/Organization');
-const Consultant = require('../../../models/Consultant');
-const Partner = require('../../../models/Partner');
+const Application = require('../../models/Application');
+const User = require('../../models/User');
+const Member = require('../../models/Member');
+const Organization = require('../../models/Organization');
+const Partner = require('../../models/Partner');
+const Consultant = require('../../models/Consultant');
+const Admin = require('../../models/Admin');
 
-// Temporary Data
+// Validation
+const validator = require('../../validation/consultantValidations');
+
+/*// Temporary Data
 const users = [
     new User('karim13','karimPassword',1),
     new User('youssef12','youssefPassword',2),
@@ -34,22 +40,34 @@ const partners = [
     new Partner('Civil Engineering', 2),
     new Partner('Graphic Design', 3),
     new Partner('Online Banking', 4),
-];
+];*/
+
+
 // @route   POST api/profiles/consultant/create/:id
 // @desc    Creates Consultant Profile
 // @access  Private
 router.post('/create/:id', (req,res)=>{
     const id = req.params.id;
     const organization = organizations.find(element => {
-        return element.id == id;
+       return element.req.params.id === req.params.id;
     });
     if(!organization)   return res.status(404).json({ profile: 'There is no Organization profile for this user' });
-    const newConsultant = {
-        id
-    };
-    consultants.push(newConsultant);
-    return res.json({ data: newConsultant });
-});
+    const isValidated = validator.editValidation(req.body);
+	 if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+	const newconsultant = new consultant({
+                id: req.body.id,
+                workPosition: req.body.workPosition,
+                status: req.body.status,
+				boardMembers: req.body.boardMembers,
+				events: req.body.events,
+				partners: req.body.partners,
+				reports: req.body.reports,
+				applications: req.body.applications,
+            });
+            newconsultant.save()
+                .then({msg:'consultant was not created  successfully', data: newconsultant});
+        })
+        
 
 
 // @route   GET api/profiles/consultant/:id
@@ -58,14 +76,14 @@ router.post('/create/:id', (req,res)=>{
 router.get('/:id',(req,res)=>{
     const id = req.params.id;
     const consultant = consultants.find(element => {
-        return element.id == id;
+       return element.req.params.id === req.params.id;
     });
     if(!consultant){
         return res.status(404).json({ profile: 'There is no Consultant profile for this user' });
     }
-    else{
+    
         return res.json({data: consultant});
-    }
+    
 
 });
 
@@ -76,48 +94,51 @@ router.get('/:id',(req,res)=>{
 router.put('/edit/:id',(req,res)=>{
     const id = req.params.id;
     const consultant = consultants.find(element => {
-        return element.id == id;
+        return element.req.params.id === req.params.id;
     });
+	const isValidated = validator.editValidation(req.body);
+            if (isValidated.error) return res.status(400).send({error: isValidated.error.details[0].message});
     if(!consultant){
         return res.status(404).json({ profile: 'There is no Consultant profile for this user' });
     }
     else{
-        return res.json({data: consultant});
-    }
-
-});
+        const consultantFields = {};
+                        consultantFields.id = req.body.id;
+                        consultantFields.workPosition = req.body.workPosition;
+                        consultantFields.status = req.body.status;
+						consultantFields.boardMembers = req.body.boardMembers;
+						consultantFields.events = req.body.events;
+						consultantFields.partners = req.body.partners;
+						consultantFields.reports = req.body.reports;
+						consultantFields.applications = req.body.applications;
+                       consultant.findOneAndUpdate(
+                            {consultant: req.params.id},
+                            {$set: consultantFields},
+                            {new: true}
+                        ).then(consultant=>{
+                            return res.json({msg: 'consultant profile Was edited Successfully',data: consultant});
+                        })
+                }
 
 
 // @route POST api/profiles/consultant/board-members/add/:id
 // @decs Adds Board Member To Consultant's Profile
 // @access private
 router.post('/board-members/add/:id',(req,res)=>{
-    const name = req.body.name;
-    const age = req.body.age;
-    const phone = req.body.phone;
-    const email = req.body.email;
-    const id = req.params.id;
+   
 
     const consultant = consultants.find(element => {
-        return element.id == id;
+       return element.req.params.id === req.params.id;
     });
     if(!consultant){
         return res.status(400).json({ profile: 'There is no Consultant profile for this user' });
     };
-    if (!name) return res.status(400).send({ err: 'Name field is required' });
-    if (!age) return res.status(400).send({ err: 'Age field is required' });
-    if (!phone) return res.status(400).send({ err: 'Phone field is required' });
-    if (!email) return res.status(400).send({ err: 'Email field is required' });
+	const isValidated = validator.addboardmembersValidation(req.body);
+            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+    { $addToSet: {boardmembers: [ req.body.boardmembers ] } };
 
 
-    const boardMember ={
-        name,
-        age,
-        phone,
-        email
-    };
-    consultant.boardMembers.push(boardMember);
-    return res.json(consultant);
+   
 });
 
 
@@ -125,28 +146,18 @@ router.post('/board-members/add/:id',(req,res)=>{
 // @decs Adds Event To Consultant's Profile
 // @access private
 router.post('/events/add/:id',(req,res)=>{
-    const eventName = req.body.eventName;
-    const description = req.body.description;
-    const date = req.body.date;
-    const id = req.params.id;
+    
 
     const consultant = consultants.find(element => {
-        return element.id == id;
+         return element.req.params.id === req.params.id;
     });
     if(!consultant){
         return res.status(400).json({ profile: 'There is no Consultant profile for this user' });
     };
+	const isValidated = validator.addeventsValidation(req.body);
+            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
 
-    if (!eventName) return res.status(400).send({ err: 'Event Name field is required' });
-    if (!description) return res.status(400).send({ err: 'Event Description field is required' });
-
-    const event = {
-        eventName,
-        description,
-        date
-    };
-    consultant.events.push(event);
-    return res.json(consultant);
+   { $addToSet: {events: [ req.body.events ] } };
 });
 
 // @route POST api/profiles/consultant/partners/add/:id/:id2
@@ -156,31 +167,26 @@ router.post('/partners/add/:id/:id2',(req,res)=>{
     const consultantID = req.params.id;
     const partnerID = req.params.id2;
     const consultant = consultants.find(element => {
-        return element.id == consultantID;
+        return element.req.params.id === req.params.consultantID;
     });
     if(!consultant){
         return res.status(400).json({ profile: 'There is no Consultant profile for this user' });
     };
     const partner = partners.find(element => {
-        return element.id == partnerID;
+        return element.req.params.id === req.params.partnerID;
     });
     if(!partner){
         return res.status(400).json({ profile: 'There is no Partner profile for this user' });
     };
     const organization = organizations.find(element => {
-        return element.id == partnerID;
+        return element.req.params.id === req.params.organizationID;
     });
     if(!organization){
         return res.status(400).json({ profile: 'There is no Organization profile for this user' });
     };
-    const myPartner = {
-        name: organization.name,
-        email: organization.email,
-        phone: organization.phone,
-        address: organization.address,
-    };
-    consultant.partners.push(myPartner);
-    return res.json(consultant);
+	const isValidated = validator.addpartnersValidation(req.body);
+            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+   { $addToSet: {events: [ req.body.events ] } };
 });
 
 
@@ -188,18 +194,18 @@ router.post('/partners/add/:id/:id2',(req,res)=>{
 // @decs Adds A Report To Consultant's Profiles
 // @access private
 router.post('/reports/add/:id',(req,res)=>{
-    const report = req.body.report;
-    const id = req.params.id;
+    
     const consultant = consultants.find(element => {
-        return element.id == id;
+        return element.req.params.id === req.params.consultantID;
     });
     if(!consultant){
         return res.status(400).json({ profile: 'There is no Consultant profile for this user' });
     };
     if (!report) return res.status(400).send({ err: 'Report field is required' });
+	const isValidated = validator.addreportsValidation(req.body);
+            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
 
-    consultant.reports.push(report);
-    return res.json(consultant);
+   { $addToSet: {events: [ req.body.events ] } };
 });
 
 // @route   DELETE api/profiles/consultant/delete/:id
@@ -208,7 +214,7 @@ router.post('/reports/add/:id',(req,res)=>{
 router.delete('/delete/:id',(req,res)=>{
     const id = req.params.id;
     const consultant = consultants.find(element => {
-        return element.id == id;
+        return element.req.params.id === req.params.consultantID;
     });
     if(!consultant){
         return res.status(404).json({ profile: 'There is no Consultant profile for this user' });
@@ -218,3 +224,4 @@ router.delete('/delete/:id',(req,res)=>{
 });
 
 module.exports = router;
+});
