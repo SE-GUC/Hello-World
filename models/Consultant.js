bf8353718218cb3977d00aa1b00e84c2e4e16be9
youssefEmadabd <youssef.emad1@yahoo.com>
2019-03-23 21:05:29 +0200

    errorless

--- models/Consultant.js
+++ models/Consultant.js
@@ -1,12 +1,50 @@
-class Consultant {
-    constructor(id) {
-        this.id = id;
-        this.boardMembers = [];
-        this.events = [];
-        this.partners = [];
-        this.reports = [];
-        this.applications = [];
-    };
-};
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const constructorSchema = new Schema({
+     workPosition: { 
+         type: String,
+         required: true},
+         boardMembers: [
+            {
+                name:{
+                    type: String,
+                    required: true
+                },
+                position:{
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+         events:[
+            {
+                title:{
+                    type: String,
+                    required: true
+                },
+                description:{
+                    type: String,
+                    required: true
+                },
+                date:{
+                    type: Date,
+                    default: Date.now()
+                }
+            }
+        ],
+         partners: {
+            type: [Schema.Types.ObjectId],
+            ref: 'partner'
+        },
+     reports: {
+         type: [String],
+         required: true},
+    applications: {
+         type: [Schema.Types.ObjectId],
+         ref:'applications'}
+})
 
-module.exports = Consultant;
+
+
+
+module.exports = Consultant= mongoose.model('consultants', constructorSchema)
