8bb1ac088313bec713d5a557e338776805da8e3f
TheKareemOne <thekareem13@gmail.com>
2019-03-23 20:59:40 +0200

    Fixed Typo in app.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
     mongoURI: 'mongodb+srv://helloWorld:helloworld@hello-world-m3qyu.mongodb.net/hello-world?retryWrites=true'
 }
-=======
-    mongoURI: 'mongodb+srv://helloWorld:helloworld@hello-world-m3qyu.mongodb.net/hello-world?retryWrites=true'}
->>>>>>> dfc52c7bc7d53c92f0163fcd9f82d43a7b1acf20
