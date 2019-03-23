a68fe972631152304d7b2950446e6c1b4c94705e
TheKareemOne <thekareem13@gmail.com>
2019-03-23 20:52:32 +0200

    Fixed Typo in Masterclass Model

--- models/Masterclass.js
+++ models/Masterclass.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
-<<<<<<< HEAD
 const MasterclassSchema = new Schema({
     name:{
         type:String,
@@ -33,22 +32,3 @@ const MasterclassSchema = new Schema({
 })
 
 module.exports = Masterclass = mongoose.model('masterclass',MasterclassSchema);
-=======
-masterclassschema = new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    applicants:[{
-         type: Schema.Types.ObjectId,
-        ref: 'members'
-    }]
-
-})
-
-module.exports = Masterclass =mongoose.model('masterclasses',masterclassschema);
->>>>>>> dfc52c7bc7d53c92f0163fcd9f82d43a7b1acf20
