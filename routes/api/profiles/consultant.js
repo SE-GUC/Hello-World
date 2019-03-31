7d672046f9f6729c9a74d1c886cf4ec8bd4b3e92
mahmoudbasha3 <mahmoudbashabsbs@gmail.com>
2019-03-31 13:51:15 +0200

    fixed some errors from sprint 2 in conultant route

--- routes/api/profiles/consultant.js
+++ routes/api/profiles/consultant.js
@@ -9,33 +9,43 @@ const Organization = require('../../../models/Organization');
 const Consultant = require('../../../models/Consultant');
 const Partner = require('../../../models/Partner');
 //validator
-const validator = require('../../../validation/applicationsValidation');
+const validator = require('../../../validation/consultantValidation');
 // @route   POST api/profiles/consultant/create/:id
 // @desc    Creates Consultant Profile
 // @access  Private
-router.post('/create/:id', (req,res)=>{
+router.post('/create/:id', async(req,res)=>{
     const id = req.params.id;
-    res.json({msg:"el3bylla"})
-    const organization = organizations.find(element => {
-        return element.id == id;
-    });
+    
+    const organization =await organizations.findById(id)
     if(!organization)   return res.status(404).json({ profile: 'There is no Organization profile for this user' });
     const newConsultant = {
         id
     };
     consultants.push(newConsultant);
+    
     return res.json({ data: newConsultant });
+    
 });
+
+/*router.get('/',async (req,res)=>{
+    const id = req.params.id;
+    const consultant =await Consultant.find();
+    if(!consultant){
+        return res.status(404).json({ profile: 'There is no Consultant profile for this user' });
+    }
+    else{
+        return res.json({data: consultant});
+    }
+
+});*/
 
 
 // @route   GET api/profiles/consultant/:id
 // @desc    Get consultant's profile by ID
 // @access  private
-router.get('/:id',(req,res)=>{
+router.get('/:id',async(req,res)=>{
     const id = req.params.id;
-    const consultant = consultants.find(element => {
-        return element.id == id;
-    });
+    const consultant =await Consultant.findById(id)
     if(!consultant){
         return res.status(404).json({ profile: 'There is no Consultant profile for this user' });
     }
@@ -68,6 +78,7 @@ router.put('/edit/:id',async(req,res)=>{
 router.post('/board-members/add/:id',async(req,res)=>{
     const {name,age,phone,email} = req.body;
     const id = req.params.id;
+   
 
     const consultant = await Consultant.findById(id)
     if(!consultant){
