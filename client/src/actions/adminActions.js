862c7700b761fd3430e9c76eddf49003325dcea5
Sohail Khattab <sohail9711@gmail.com>
2019-04-07 19:07:05 +0200

    admin view all applications

---
+++ client/src/actions/adminActions.js
@@ -0,0 +1,12 @@
+import { GET_APPLICATIONS } from "./types";
+const fetch = require("node-fetch");
+
+export const getApplications = id => async dispatch => {
+  const res = await fetch(`http://localhost:5000/api/applications/admin/${id}`);
+
+  const json = await res.json();
+  dispatch({
+    type: GET_APPLICATIONS,
+    payload: json.data
+  });
+};
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,2 @@
 export const GET_EO = "GET_EO";
+export const GET_APPLICATIONS = "GET_APPLICATIONS";
