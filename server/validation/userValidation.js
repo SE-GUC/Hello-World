366566bba6752a6512f4062e75161e03ee23ba46
TheKareemOne <thekareem13@gmail.com>
2019-04-10 17:24:56 +0200

    Implemented Register and Login in server

---
+++ server/validation/userValidation.js
@@ -0,0 +1,25 @@
+const Joi = require("joi");
+
+module.exports = {
+  registerValidation: request => {
+    const registerSchema = {
+      username: Joi.string().required(),
+      password: Joi.string()
+        .min(8)
+        .required(),
+      password2: Joi.string()
+        .min(8)
+        .required()
+    };
+
+    return Joi.validate(request, registerSchema);
+  },
+  loginValidation: request => {
+    const loginSchema = {
+      username: Joi.string().required(),
+      password: Joi.string().required()
+    };
+
+    return Joi.validate(request, loginSchema);
+  }
+};
