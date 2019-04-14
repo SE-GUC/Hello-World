bca3059f6f3c8a9f42110a0242ed1ed54de6309d
TheKareemOne <thekareem13@gmail.com>
2019-04-14 20:20:10 +0200

    Fixed Typo in app.js

--- server/app.js
+++ server/app.js
@@ -66,7 +66,7 @@ app.use("/api/masterclasses", masterclasses);
 app.use("/api/applications", applications);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
