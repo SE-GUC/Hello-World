c1765a31b457507f36e9b000c15cdfb69bf49493
mahmoudbasha3 <mahmoudbashabsbs@gmail.com>
2019-04-14 16:06:06 +0200

    a consultant cn create a profile now

--- client/src/actions/consultantActions.js
+++ client/src/actions/consultantActions.js
@@ -8,8 +8,37 @@ export const getConsultant = id => async dispatch => {
   );
 
   const json = await res.json();
-  dispatch({
-    type: GET_CONSULTANT,
-    payload: json.data
+  if (json.data) {
+    dispatch({
+      type: GET_MEMBER,
+      payload: json.data
+    });
+  } else {
+    dispatch({
+      type: GET_MEMBER,
+      payload: {}
+    });
+  }
+};
+
+// Create Consultant Profile
+export const createConsultant = (consultantData, history) => async dispatch => {
+  const body = JSON.stringify(consultantData);
+  const res = await fetch("http://localhost:5000/api/profiles/consultant", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwtToken")
+    },
+    body: body
   });
+  const json = await res.json();
+  if (json.data) {
+    history.push("/dashboard");
+  } else {
+    dispatch({
+      type: GET_ERRORS,
+      payload: json
+    });
+  }
 };
