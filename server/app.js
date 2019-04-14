7988b2ac1fef68a5b0572383b59974fa05625e9c
TheKareemOne <thekareem13@gmail.com>
2019-04-14 20:15:24 +0200

    Fixed typo in app.js

--- server/app.js
+++ server/app.js
@@ -65,11 +65,10 @@ app.use("/api/profiles/education", education);
 app.use("/api/masterclasses", masterclasses);
 app.use("/api/applications", applications);
 
-// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
